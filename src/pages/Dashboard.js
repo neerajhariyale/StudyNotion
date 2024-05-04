356eed78ad02e85f20df052e6a41e0c748744c79
Neeraj Hariyale <115395529+neerajhariyale@users.noreply.github.com>
2024-05-04 21:12:29 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className='flex justify-center items-center flex-col h-full text-center text-3xl text-white'>
+    <div className='flex justify-center items-center flex-col h-screen text-center text-3xl text-white'>
       <h1>Welcome to Dashboard</h1>
     </div>
   )
